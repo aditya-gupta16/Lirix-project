14aaaf1d6b8433ebcf0a51f4be7af5a3ce117a62
Ritik soni <sonyritek@gmail.com>
2024-07-09 23:21:30 +0530

    loader commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-hello()={
-    
-}
+gsap.to(".ldr_overlay_page_1", {
+    top: 0,
+    duration: .5,
+    delay: 1
+})
+gsap.to(".ldr_overlay_page_2", {
+    top: 0,
+    duration: .5,
+    delay: 1.3
+})
