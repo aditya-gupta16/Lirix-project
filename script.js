ca5828428bab80280bffaf6f1d7e2a08d1629d44
Aditya Gupta <adityagupta90395@gmail.com>
2024-07-13 17:36:16 +0530

    second update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 gsap.to(".ldr_overlay_page_1", {
     top: 0,
     duration: .5,
@@ -7,4 +9,13 @@ gsap.to(".ldr_overlay_page_2", {
     top: 0,
     duration: .5,
     delay: 1.3
+})
+
+var main = document.querySelector(".main")
+
+gsap.to(main, {
+    y: -750,
+    opacity: 1,
+    duration: 1,
+    
 })
